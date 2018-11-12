5f8393b4444c1d314b21494bfe0315fa61ae91a9
Mireczko <walawender.kuba@gmail.com>
2018-11-12 22:05:57 +0100

    Added GET for ID

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb');
 let {mongoose} = require('./db/mongoose');
 let {Task} = require('./models/Task');
 let {User} = require('./models/User');
@@ -25,7 +25,23 @@ app.get('/Tasks', (req, res) =>{
     Task.find().then((tasks) => {
         res.send({tasks});
     }, (e) => {
-        res.status(400).send(e);
+        res.status(404).send(e);
+    });
+});
+
+app.get('/Tasks/:id', (req, res) => {
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)){
+       return res.status(400).send();
+    }
+    Task.findById(id).then((task) => {
+        if(!task){
+            return res.status(404).send();
+        }
+
+        res.send({task});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
